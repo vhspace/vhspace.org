7b066ebf98240ba2957c2f7b9c6c54ece0c511c7
Mischa Spiegelmock <me@mish.dev>
2022-11-06 17:47:53 -0800

    cloudfront + route53 + acm

--- stacks/CloudFront.ts
+++ stacks/CloudFront.ts
@@ -1,12 +1,38 @@
-import { StackContext, Api } from "@serverless-stack/resources";
+import { StackContext, use } from "@serverless-stack/resources";
+import { Distribution } from "aws-cdk-lib/aws-cloudfront";
+import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { AaaaRecord, ARecord, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
+import { Route53 } from "./Route53";
 
 export function CloudFront({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
-    routes: {
-      "GET /": "functions/lambda.handler",
+  const { certificateGlobal, domainName, zone } = use(Route53);
+
+  const distribution = new Distribution(stack, "HomeSiteDistribution", {
+    defaultBehavior: {
+      origin: new HttpOrigin("xn--yu8h.notion.site", {
+        originPath:
+          "/What-is-a-Virtual-HackerSpace-2f6b96d139e144ef8b3677c2852db910",
+      }),
     },
+
+    certificate: certificateGlobal,
+    domainNames: [domainName, `www.${domainName}`],
+    comment: `Home site for ${domainName}`,
+    enableIpv6: true,
   });
-  stack.addOutputs({
-    ApiEndpoint: api.url,
+
+  // create DNS records
+  new ARecord(stack, "HomeSiteARecord", {
+    zone,
+    target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
   });
+  new AaaaRecord(stack, "HomeSiteAaaaRecord", {
+    zone,
+    target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+  });
+
+  return {
+    distribution,
+  };
 }
---
+++ stacks/Route53.ts
@@ -0,0 +1,26 @@
+import { StackContext } from "@serverless-stack/resources";
+import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
+import { HostedZone, ARecord, RecordTarget } from "aws-cdk-lib/aws-route53";
+
+/**
+ * Our DNS zones.
+ */
+export function Route53({ stack }: StackContext) {
+  const zoneName = process.env.HOSTED_ZONE;
+  if (!zoneName) throw new Error("HOSTED_ZONE is not set");
+  const zone = new HostedZone(stack, "HomeSiteZone", { zoneName });
+
+  // request a certificate with ACM
+  const certificateGlobal = new DnsValidatedCertificate(
+    stack,
+    "HomeSiteCertificateGlobal",
+    {
+      domainName: zoneName,
+      subjectAlternativeNames: [`*.${zoneName}`],
+      hostedZone: zone,
+      region: "us-east-1",
+    }
+  );
+
+  return { zone, certificateGlobal, domainName: zoneName };
+}
